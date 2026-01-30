5ef07ed665edabc74eb90f5745c9d31c278a5fb5
Fabio Cingolani <fabiocin@Fabios-Mac-Studio.local>
2026-01-30 09:50:57 +0100

    added middleware for security for new apis

--- api/discord/index.js
+++ api/discord/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { requireAuth } from "../../middleware/auth.js";
+import { requireDM } from "../../middleware/auth.js";
 import { User } from "../../models.js";
 import discordClient, {
   initializeDiscordClient,
